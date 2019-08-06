334087e981a4678897c9831b6625dcbab3e2ca9e
Julia Moses <jslmoses@gmail.com>
2019-08-06 12:02:53 -0400

    definition of word now entered when writen in command line

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import json
 
+# app.py is opening & reading dictionary data
 data = json.load(open("data.json"))
 
-def translate(word):
-	return data[word]
+# returns definition of word
+def translate(w):
+	# if the word is in json dictionary, will give definition
+	if w in data:
+		return data[w]
+	else:
+		return "Sorry! this word isn't in the dictionary, please double check it."
 
 word = input("Enter a word: ")
 
